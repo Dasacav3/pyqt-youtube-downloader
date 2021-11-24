773bb51fea16fc0190a7ae2d069e3c1b0af14676
Dasacav3 <dscv3719@gmail.com>
2021-11-23 21:39:28 -0500

    Added more methods to the YT_Downloader class

--- yt_dl.py
+++ yt_dl.py
@@ -38,4 +38,72 @@ class YT_Downloader():
                 ydl.download([url])
             print("Downloaded")
         except:
-            print("Error downloading mp3")
+            print("Error downloading mp4")
+
+    def download_aac(self, url, path):
+        try:
+            ydl_opts = {
+                'format': 'bestaudio/best',
+                'outtmpl': path + '%(title)s.%(ext)s',
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'aac',
+                    'preferredquality': '192',
+                }],
+            }
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([url])
+            print("Downloaded")
+        except:
+            print("Error downloading aac")
+
+    def download_webm(self, url, path):
+        try:
+            ydl_opts = {
+                'format': 'bestvideo[ext=webm]+bestaudio[ext=webm]/best[ext=webm]/best',
+                'outtmpl': path + '%(title)s.%(ext)s',
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '192',
+                }],
+            }
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([url])
+            print("Downloaded")
+        except:
+            print("Error downloading webm")
+
+    def download_ogg(self, url, path):
+        try:
+            ydl_opts = {
+                'format': 'bestaudio/best',
+                'outtmpl': path + '%(title)s.%(ext)s',
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'ogg',
+                    'preferredquality': '192',
+                }],
+            }
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([url])
+            print("Downloaded")
+        except:
+            print("Error downloading ogg")
+        
+    def download_m4a(self, url, path):
+        try:
+            ydl_opts = {
+                'format': 'bestaudio/best',
+                'outtmpl': path + '%(title)s.%(ext)s',
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'm4a',
+                    'preferredquality': '192',
+                }],
+            }
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([url])
+            print("Downloaded")
+        except:
+            print("Error downloading m4a")
